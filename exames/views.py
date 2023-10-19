ab4037381e89fd4d602c53794f51b316d736aa41
Henrique <henrique.mory@hotmail.com>
2023-10-18 23:54:35 -0300

    Pequenas alterações

--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def solicitar_exames(request):
+    #if not request.user.is_authenticated:
+        #return HttpResponse("VocÊ não está logado!")
+    return HttpResponse('Estou aqui')
