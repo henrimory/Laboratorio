1e06f2e0ec62b157599ce19c39c5774dcdcf0efa
Henrique <henrique.mory@hotmail.com>
2023-10-22 01:14:47 -0300

    Tela de solicitação de exames

--- exames/views.py
+++ exames/views.py
@@ -2,9 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import TiposExames
 
 @login_required
 def solicitar_exames(request):
+    tipos_exames = TiposExames.objects.all() #-> Opção para trazer todos os exames
     #if not request.user.is_authenticated:
         #return HttpResponse("VocÊ não está logado!")
-    return HttpResponse('Estou aqui')
+    #return HttpResponse('Estou aqui')
+    if request.method == "GET":        
+        #for i in tipos:
+            #print(i)
+         #   print(i.nome)
+         #   print(i.preco)
+         #   print('-' * 10)
+        #TiposExames.objects.filter() -> Opção para filtrar um exame específico
+        return render(request, 'solicitar_exames.html', {'tipos_exames':tipos_exames})
+    elif request.method == "POST":
+        exames_id = request.POST.getlist('exames')
+        solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+        print(solicitacao_exames)
+
+        #TODO: verificar o preço dos dados disponiveis
+        preco_total = 0
+        for i in solicitacao_exames:
+            if i.disponivel:
+                preco_total += i.preco
+        
+        return render(request, 'solicitar_exames.html', {'tipos_exames':tipos_exames,
+                                                          'solicitacao_exames': solicitacao_exames,
+                                                          'preco_total':preco_total})
